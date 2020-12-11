25438c38a907a861dc294425c01163e2db354602
William (Wgyt) <wgyt@usa.com>
2020-12-10 20:25:34 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setUsername('wgytbot');
+	client.user.setUsername('wgytbotbeta');
     client.user.setPresence({
         status: "online",  //You can show online, idle....
         game: {
-	    url:"https://bot.wgyt.tk/",
-            name: "commands.",  //The message shown
+	    url:"https://josh-bot-staging.herokuapp.com/",
+            name: "commands. BETA VERSION. https://josh-bot-staging.herokuapp.com/",  //The message shown
             type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
         }
     });}
